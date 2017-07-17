8c7cd3342852e3a7f7c454e7330023f1c5c027bc
Bob <hanssepp0@gmx.de>
2017-07-17 12:22:21 +0200

    Code refactored

--- app/src/main/java/com/bowazik/bob/ibet/network/LoginAsyncTask.java
+++ app/src/main/java/com/bowazik/bob/ibet/network/LoginAsyncTask.java
@@ -3,12 +3,7 @@ package com.bowazik.bob.ibet.network;
 import android.os.AsyncTask;
 import android.util.Log;
 
-import com.bowazik.bob.ibet.data.iBet;
 import com.bowazik.bob.ibet.utility.Constants;
-import com.bowazik.bob.ibet.utility.IbetUtility;
-
-import org.json.JSONArray;
-import org.json.JSONException;
 
 import java.io.IOException;
 import java.io.OutputStreamWriter;
@@ -17,8 +12,6 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by bob on 13.07.17.
@@ -40,13 +33,13 @@ public class LoginAsyncTask extends AsyncTask<String, Void, Boolean> {
         int responseCode = 0;
         String url = Constants.IBET_SERVER_PHP_URL_CHECK_USER, serverResponse, data = null, username = userData[0], password = userData[1];
         HttpURLConnection httpURLConnection = null;
-        OutputStreamWriter outputStreamWriter = null;
+        OutputStreamWriter outputStreamWriter;
         URL server = null;
 
         try{
             data = URLEncoder.encode("username" , "UTF-8") + "=" + URLEncoder.encode(username, "UTF-8");
             data += "&" + URLEncoder.encode("password" , "UTF-8") + "=" + URLEncoder.encode(password, "UTF-8");
-            Log.v(TAG, "Data: "+data);
+            Log.v(TAG, "Login Data: "+data);
         }catch(UnsupportedEncodingException e){
             e.printStackTrace();
         }
